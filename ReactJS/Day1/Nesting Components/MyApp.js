51e2aa285737945661b46f8321e6ecd1cb9098b5
Mahudesh <115523760+Mahudesh@users.noreply.github.com>
2024-07-02 22:05:39 +0530

    Create MyApp.js

---
+++ ReactJS/Day1/Nesting Components/MyApp.js
@@ -0,0 +1,14 @@
+
+import MyButton from "./MyButton";
+
+function MyApp()
+{
+    return (
+        <div>
+            <h1>Welcome To Dev Cafe</h1>
+            <h1>Admin Panel</h1>
+            <MyButton/>
+        </div>
+    );
+}
+export default MyApp;
