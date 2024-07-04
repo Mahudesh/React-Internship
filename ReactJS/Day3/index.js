8c72b8cee8ed3850d26bc43bf7bec8b31ebe8dc0
Mahudesh <115523760+Mahudesh@users.noreply.github.com>
2024-07-04 17:02:51 +0530

    Create index.js

---
+++ ReactJS/Day3/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+import RenderingLists from './RenderingLists';
+import Counter from './Counter';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Counter/>
+  </React.StrictMode>
+);
+
+reportWebVitals();
